c3edf623bf9f85814db6eee2ac034ec5904646bb
Kye Davey <kye.j.davey@gmail.com>
2025-05-14 21:43:08 +1000

    link configs with stow

--- install.sh
+++ install.sh
@@ -14,4 +14,10 @@ git clone https://github.com/kyedavey/${REPO_NAME}.git ${REPO_LOCAL_LOCATION}
 echo -e "\nInstallation starting..."
 for installer in ${REPO_LOCAL_LOCATION}/install/*.sh; do source $installer; done
 
+echo -e "\nLinking configuration files..."
+cd ${REPO_LOCAL_LOCATION}/config
+stow --adopt -t ~/ .
+git reset --hard
+cd -
+
 echo -e "\nInstallation complete"
