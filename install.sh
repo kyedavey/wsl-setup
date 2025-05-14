da4e0543bb81c9384b6e9935288499f7f96fdd9f
Kye Davey <kye.j.davey@gmail.com>
2025-05-14 17:43:59 +1000

    add git

---
+++ install.sh
@@ -0,0 +1,12 @@
+set -e
+
+REPO_NAME="wsl-setup"
+REPO_LOCAL_LOCATION="${HOME}/code/${REPO_NAME}"
+
+echo -e "\nCloning ${REPO_NAME}..."
+git clone "https://github.com/kyedavey/${REPO_NAME}.git" "${REPO_LOCAL_LOCATION}"
+
+echo -e "\nInstallation starting..."
+for installer in "${REPO_LOCAL_LOCATION}/install/*.sh"; do source $installer; done
+
+echo -e "\nInstallation complete"
---
+++ install/git.sh
@@ -0,0 +1,4 @@
+echo -e "\nInstalling Git..."
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install -y git
